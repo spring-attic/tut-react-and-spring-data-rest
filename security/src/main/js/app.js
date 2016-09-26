f481981987de240c142de26a274b589a4b912dd9
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2016-09-26 13:26:43 +0700

    migrate to 15.3.2 change React.findDOMNode to ReactDOM.findDOMNode

--- security/src/main/js/app.js
+++ security/src/main/js/app.js
@@ -244,11 +244,11 @@ class CreateDialog extends React.Component {
 		e.preventDefault();
 		var newEmployee = {};
 		this.props.attributes.forEach(attribute => {
-			newEmployee[attribute] = React.findDOMNode(this.refs[attribute]).value.trim();
+			newEmployee[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
 		});
 		this.props.onCreate(newEmployee);
 		this.props.attributes.forEach(attribute => {
-			React.findDOMNode(this.refs[attribute]).value = ''; // clear out the dialog's inputs
+			ReactDOM.findDOMNode(this.refs[attribute]).value = ''; // clear out the dialog's inputs
 		});
 		window.location = "#";
 	}
@@ -291,7 +291,7 @@ class UpdateDialog extends React.Component {
 		e.preventDefault();
 		var updatedEmployee = {};
 		this.props.attributes.forEach(attribute => {
-			updatedEmployee[attribute] = React.findDOMNode(this.refs[attribute]).value.trim();
+			updatedEmployee[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
 		});
 		this.props.onUpdate(this.props.employee, updatedEmployee);
 		window.location = "#";
@@ -343,11 +343,11 @@ class EmployeeList extends React.Component {
 
 	handleInput(e) {
 		e.preventDefault();
-		var pageSize = React.findDOMNode(this.refs.pageSize).value;
+		var pageSize = ReactDOM.findDOMNode(this.refs.pageSize).value;
 		if (/^[0-9]+$/.test(pageSize)) {
 			this.props.updatePageSize(pageSize);
 		} else {
-			React.findDOMNode(this.refs.pageSize).value = pageSize.substring(0, pageSize.length - 1);
+			ReactDOM.findDOMNode(this.refs.pageSize).value = pageSize.substring(0, pageSize.length - 1);
 		}
 	}
 
