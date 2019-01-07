3a21453bf852d078459c45213ceb7a0f5c209245
DaiNiu <1244453736@qq.com>
2019-01-07 14:20:31 +0800

    run better in VScode.

--- hypermedia/src/main/java/com/greglturnquist/payroll/Employee.java
+++ hypermedia/src/main/java/com/greglturnquist/payroll/Employee.java
@@ -18,14 +18,13 @@ package com.greglturnquist.payroll;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
-import lombok.Data;
 
 /**
  * @author Greg Turnquist
  */
 // tag::code[]
-@Data
 @Entity
 public class Employee {
 
@@ -34,7 +33,69 @@ public class Employee {
 	private String lastName;
 	private String description;
 
-	private Employee() {}
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == this)
+			return true;
+		if (!(o instanceof Employee)) {
+			return false;
+		}
+		Employee employee = (Employee) o;
+		return Objects.equals(id, employee.id) && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(description, employee.description);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, firstName, lastName, description);
+	}
+
+
+	@Override
+	public String toString() {
+		return "{" +
+			" id='" + getId() + "'" +
+			", firstName='" + getFirstName() + "'" +
+			", lastName='" + getLastName() + "'" +
+			", description='" + getDescription() + "'" +
+			"}";
+	}
+
+	Employee() {
+		
+	}
 
 	public Employee(String firstName, String lastName, String description) {
 		this.firstName = firstName;
