8963efecfe50a6c3a43bdcd480d20e2059567e2b
Katherine Pully <kpully@gmail.com>
2020-03-26 18:18:26 -0500

    added jpa annotation
    page is loading but grid items are not

--- events/src/main/java/com/greglturnquist/payroll/GridItem.java
+++ events/src/main/java/com/greglturnquist/payroll/GridItem.java
@@ -1,17 +1,15 @@
 package com.greglturnquist.payroll;
 import java.util.Objects;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Version;
+import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class GridItem {
     private @Id @GeneratedValue Long id;
-    private Contestant girl;
-    private Contestant boy;
+    private @ManyToOne
+    Contestant girl;
+    private @ManyToOne Contestant boy;
     private @Version @JsonIgnore Long version;
     private int colorState;
 
